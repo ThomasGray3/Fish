2b0a583c5fc3bb578ce66542559c3b017cc09c82
Thomas Gray <tomalexander1999@googlemail.com>
2025-07-14 22:46:20 +0100

    Initial Commit

---
+++ Fish/FishApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishApp.swift
+//  Fish
+//
+//  Created by Thomas Gray on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishTests/FishTests.swift
@@ -0,0 +1,16 @@
+//
+//  FishTests.swift
+//  FishTests
+//
+//  Created by Thomas Gray on 14/07/2025.
+//
+
+import Testing
+
+struct FishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
